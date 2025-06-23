6cf365f753b2ae214c9f3c5d24e4bf5a3a627113
sbx_user1051 <sbx_user1051@169.254.15.231>
2025-06-23 01:48:04 +0000

    Mon Jun 23 01:48:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@ from collections import Counter
 class CheckoutSolution:
 
     # skus = unicode string
-    def checkout(self, skus):
+    def checkout( skus):
         prices = {
             'A' : 50,
             'B' : 30,
@@ -18,34 +18,37 @@ class CheckoutSolution:
 
         }
 
+        if not all(char in prices for char in skus):
+            return -1
+
+
+
         total = 0
         counts = Counter(skus)
 
-        for item, count in counts.items():
-            if item not in prices:
-                return -1 #invalid item
 
-            if 'E' in counts and 'B' in counts:
-                free_b = counts['E'] // 2
-                counts['B'] = max(0, counts['B'] - free_b)
+        if 'E' in counts and 'B' in counts:
+            free_b = counts['E'] // 2
+            counts['B'] = max(0, counts['B'] - free_b)
 
-            if 'A' in counts:
-                a_count = counts['A']
-                num_5A = a_count // 5
-                rem_after_5A = a_count % 5
-                num_3A = rem_after_5A // 3
-                rem_after_3A = rem_after_5A % 3
-                total += num_5A * 200 + num_3A * 130 + rem_after_3A * prices['A']
-                counts['A'] = 0
+        if 'A' in counts:
+            a_count = counts['A']
+            num_5A = a_count // 5
+            rem_after_5A = a_count % 5
+            num_3A = rem_after_5A // 3
+            rem_after_3A = rem_after_5A % 3
+            total += num_5A * 200 + num_3A * 130 + rem_after_3A * prices['A']
+            counts['A'] = 0
 
-            if 'B' in counts:
-                b_count = counts['B']
-                total += (b_count // 2) * 45 + (b_count % 2) * prices['B']
+        if 'B' in counts:
+            b_count = counts['B']
+            total += (b_count // 2) * 45 + (b_count % 2) * prices['B']
 
-            for item in['C','D','E']:
-                total += counts[item] * prices[item]
+        for item in['C','D','E']:
+            total += counts[item] * prices[item]
 
-            return total
+        return total
+
 
 
 
