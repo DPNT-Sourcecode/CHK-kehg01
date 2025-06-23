cb97538cc9d669e93edad2ede534543e33be6888
sbx_user1051 <sbx_user1051@169.254.15.231>
2025-06-23 01:31:19 +0000

    Mon Jun 23 01:31:19 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,36 @@
+from collections import Counter
 
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        prices = {
+            'A' : 50,
+            'B' : 30,
+            'C' : 20,
+            'D' : 15
+        }
+
+        offers = {
+            'A' : (3, 130), #3A for 130
+            'B' : (2,45) # 2B for 45
+        }
+
+        total = 0
+        counts = Counter(skus)
+
+        for item, count in counts.items():
+            if item not in prices:
+                return -1 #invalid item
+
+            if item in offers:
+                offer_qty, offer_prices = offers[item]
+                total += (count // offer_qty) * offer_prices
+                total +=(count % offer_qty) * prices[item]
+            
+            else:
+                total += count * prices[item]
+
+        return total
+
+
