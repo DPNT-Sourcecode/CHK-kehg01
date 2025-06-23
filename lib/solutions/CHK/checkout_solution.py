5285b2bc1b9ef3864356caf69d209e667c5f6193
sbx_user1051 <sbx_user1051@169.254.15.231>
2025-06-23 02:19:05 +0000

    Mon Jun 23 02:19:05 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,32 +5,8 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         prices = {
-            'A' : 50,
-            'B' : 30,
-            'C' : 20,
-            'D' : 15,
-            'E' : 40, #2E get one B for free,
-            'F' : 10,
-            'G' :  20,
-            'H' : 10,
-            'I' : 35 ,
-            'J ' : 60 ,
-            'K ' : 80 ,
-            'L ' : 90,
-            'M' : 15 ,
-            'N' : 40 ,
-            'O' : 10 ,
-            'P'  :50,
-            'Q ' : 30 ,
-             'R ' : 50 ,
-            ' S ' : 30 ,
-            'T' : 20 ,
-            'U' : 40 ,
-            'V' : 50 ,
-             'W': 20 ,
-            'X': 90 ,
-            'Y': 10 ,
-            'Z': 50
+            'A' : 50,'B' : 30,'C' : 20, 'D' : 15,'E' : 40,'F' : 10, 'G' :  20,'H' : 10, 'I' : 35 , 'J' : 60 ,'K' : 80 ,'L' : 90, 'M' : 15 ,  'N' : 40 ,
+            'O' : 10 , 'P'  :50,'Q ' : 30 ,    'R ' : 50 ,   ' S ' : 30 ,    'T' : 20 ,    'U' : 40 ,   'V' : 50 ,   'W': 20 ,   'X': 90 ,   'Y': 10 ,  'Z': 50
 
         }
 
@@ -79,7 +55,7 @@ class CheckoutSolution:
         for item, deals in offers.items():
             if item in counts:
                 qty = counts[item]
-                for deal_qty, deal_price in deals:
+                for deal_qty, deal_price in sorted(deals, reverse=True):
                     deal_count = qty //deal_qty
                     total+= deal_count * deal_price
                     qty %= deal_qty
@@ -92,6 +68,7 @@ class CheckoutSolution:
             total += qty * prices[item]
 
         return total
+
 
 
 
